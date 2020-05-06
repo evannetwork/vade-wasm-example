63d840c147b3f5e61032fdef0974afa519ccbbfd
wulfraem <wulfraem@users.noreply.github.com>
2020-05-06 17:04:46 +0200

    add `vade` usage example

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 mod utils;
 
+use vade::Vade;
+use vade_evan::plugin::rust_vcresolver_evan::RustVcResolverEvan;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::future_to_promise;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -16,4 +19,20 @@ extern {
 #[wasm_bindgen]
 pub fn greet() {
     alert("Hello, vade-wasm-example!");
+}
+
+#[wasm_bindgen]
+pub fn get_vc_document(vc_name: JsValue) -> js_sys::Promise  {
+    future_to_promise(async move {
+        // create new vade instance with VC resolver
+        let rde = RustVcResolverEvan::new();
+        let mut vade = Vade::new();
+        vade.register_vc_resolver(Box::from(rde));
+    
+        // fetch document
+        let vc_document = vade.get_vc_document(&vc_name.as_string().unwrap()).await.unwrap();
+
+        // convert to JsValue and return
+        Ok(JsValue::from(&vc_document))
+    })
 }
--- www/index.js
+++ www/index.js
@@ -1,4 +1,14 @@
 import * as wasm from "vade-wasm-example";
 
-wasm.greet();
-
+(async () => {
+    // fetch VC
+    const vc = prompt("vc", "vc:evan:testcore:0x75956ef9b3ea7d7230cf007b8ee042bcaa2a4dad8c043fa77ecf51262ee4f7a9");
+    const vcDocument = await wasm.get_vc_document(vc);
+    
+    // render VC
+    const div = document.createElement('div');
+    div.style['white-space'] = 'pre-wrap';
+    div.innerText = JSON.stringify(JSON.parse(vcDocument), null, 2);
+    document.body.appendChild(div);
+    console.dir(JSON.parse(vcDocument));
+})();
